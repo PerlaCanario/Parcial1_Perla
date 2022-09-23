db68471b55b6cc547fb460c5b2fcb7744dc4c9ff
Perla D. Canario Perez <perladenis163@gmail.com>
2022-09-22 23:56:44 -0400

    Agregando el appModule

---
+++ app/src/main/java/edu/ucne/parcial1_perla/di/AppModule.kt
@@ -0,0 +1,23 @@
+package edu.ucne.parcial1_perla.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+object AppModule {
+    @Singleton
+    @Provides
+    fun ProvideParcialBd(@ApplicationContext context: Context): ParcialBd {
+        val DATABASE_NAME = "examenBd"
+        return Room.databaseBuilder(
+            context,
+            ParcialBd::class.java,
+            DATABASE_NAME       )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+
+}
