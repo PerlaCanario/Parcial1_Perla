ff19e08ad58ba14bdd0e0c2b2b04ce68a9beffa3
Perla D. Canario Perez <perladenis163@gmail.com>
2022-09-22 23:54:35 -0400

    Creando clase composable

---
+++ app/src/main/java/edu/ucne/parcial1_perla/navegation/App.kt
@@ -0,0 +1,23 @@
+package edu.ucne.parcial1_perla.navegation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.parcial1_perla.ui.theme.Parcial.ConsultaParcialScreen
+import edu.ucne.parcial1_perla.ui.theme.Parcial.RegistroParcialScreen
+
+@Composable
+fun MyApp() {
+    val navHostController = rememberNavController()
+
+    NavHost(navController = navHostController, startDestination = Screen.ConsultaParcialScreen.route){
+        composable(Screen.ConsultaParcialScreen.route){
+            ConsultaParcialScreen(navHostController = navHostController)
+        }
+        composable(Screen.RegistroParcialScreen.route){
+            RegistroParcialScreen(navHostController = navHostController)
+        }
+
+    }
+}
