87fd9a4d022059231444a50a513d6e505eaf8a6b
Perla D. Canario Perez <perladenis163@gmail.com>
2022-09-23 00:01:03 -0400

    Agregando la clase

---
+++ app/src/main/java/edu/ucne/parcial1_perla/ui/theme/Parcial/ParcialViewModel.kt
@@ -0,0 +1,14 @@
+package edu.ucne.parcial1_perla.ui.theme.Parcial
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ParcialViewModel @Inject constructor(
+) : ViewModel(){
+
+    fun Guardar(){
+    }
+
+}
