74dfeafc04554fcdde12bd6b8461b2fe0e2601a6
Perla D. Canario Perez <perladenis163@gmail.com>
2022-09-28 20:00:43 -0400

    Corrigiendo AppModule

--- app/src/main/java/edu/ucne/parcial1_perla/di/AppModule.kt
+++ app/src/main/java/edu/ucne/parcial1_perla/di/AppModule.kt
@@ -2,10 +2,17 @@ package edu.ucne.parcial1_perla.di
 
 import android.content.Context
 import androidx.room.Room
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import edu.ucne.parcial1_perla.data.ArticuloDb
 import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 
 object AppModule {
     @Singleton
